7b117222ab3ad2983487c8c91a4f99b8c65379de
zhufl0811 <zhufengliang0811@163.com>
2023-09-14 17:32:44 +0800

    vue create

---
+++ ui/src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="height:100%">
+    <h1>LoginView</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView'
+}
+</script>
---
+++ ui/src/views/model.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+  <h1> YoloModel </h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'YoloModel'
+}
+</script>
