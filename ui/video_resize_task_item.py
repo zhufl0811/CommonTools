bec520f76b1d22f64ea5f224241328fcfdbc06c9
zhufl0811 <zhufengliang0811@163.com>
2023-09-25 17:32:04 +0800

    'video-resize'

--- ui/video_resize_task_item.py
+++ ui/video_resize_task_item.py
@@ -17,96 +17,83 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(804, 71)
-        self.layoutWidget = QWidget(Form)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(50, 0, 741, 71))
-        self.horizontalLayout_3 = QHBoxLayout(self.layoutWidget)
-        self.horizontalLayout_3.setSpacing(16)
+        Form.resize(804, 67)
+        self.widget = QWidget(Form)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(40, 10, 751, 51))
+        self.horizontalLayout_3 = QHBoxLayout(self.widget)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout = QVBoxLayout()
-        self.verticalLayout.setSpacing(2)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.labelFileName = QLabel(self.layoutWidget)
+        self.labelFileName = QLabel(self.widget)
         self.labelFileName.setObjectName(u"labelFileName")
         self.labelFileName.setLineWidth(3)
         self.labelFileName.setAlignment(Qt.AlignCenter)
         self.labelFileName.setWordWrap(True)
 
-        self.verticalLayout.addWidget(self.labelFileName)
+        self.horizontalLayout_3.addWidget(self.labelFileName)
 
-        self.labelVideoInfo = QLabel(self.layoutWidget)
+        self.labelVideoInfo = QLabel(self.widget)
         self.labelVideoInfo.setObjectName(u"labelVideoInfo")
         self.labelVideoInfo.setAlignment(Qt.AlignCenter)
         self.labelVideoInfo.setWordWrap(True)
 
-        self.verticalLayout.addWidget(self.labelVideoInfo)
+        self.horizontalLayout_3.addWidget(self.labelVideoInfo)
 
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.horizontalLayout_3.addLayout(self.verticalLayout)
+        self.horizontalLayout_3.addItem(self.horizontalSpacer)
 
-        self.checkBoxKeepRatio = QCheckBox(self.layoutWidget)
-        self.checkBoxKeepRatio.setObjectName(u"checkBoxKeepRatio")
-
-        self.horizontalLayout_3.addWidget(self.checkBoxKeepRatio)
-
-        self.verticalLayout_2 = QVBoxLayout()
-        self.verticalLayout_2.setSpacing(2)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.label_3 = QLabel(self.layoutWidget)
+        self.label_3 = QLabel(self.widget)
         self.label_3.setObjectName(u"label_3")
 
         self.horizontalLayout.addWidget(self.label_3)
 
-        self.comboBoxRatio = QComboBox(self.layoutWidget)
+        self.comboBoxRatio = QComboBox(self.widget)
         self.comboBoxRatio.setObjectName(u"comboBoxRatio")
 
         self.horizontalLayout.addWidget(self.comboBoxRatio)
 
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.horizontalLayout_3.addLayout(self.horizontalLayout)
 
         self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setSpacing(6)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.lineEditResizeW = QLineEdit(self.layoutWidget)
+        self.lineEditResizeW = QLineEdit(self.widget)
         self.lineEditResizeW.setObjectName(u"lineEditResizeW")
 
         self.horizontalLayout_2.addWidget(self.lineEditResizeW)
 
-        self.label_4 = QLabel(self.layoutWidget)
+        self.label_4 = QLabel(self.widget)
         self.label_4.setObjectName(u"label_4")
 
         self.horizontalLayout_2.addWidget(self.label_4)
 
-        self.lineEditResizeH = QLineEdit(self.layoutWidget)
+        self.lineEditResizeH = QLineEdit(self.widget)
         self.lineEditResizeH.setObjectName(u"lineEditResizeH")
 
         self.horizontalLayout_2.addWidget(self.lineEditResizeH)
 
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout_3.addLayout(self.horizontalLayout_2)
 
+        self.labelState = QLabel(self.widget)
+        self.labelState.setObjectName(u"labelState")
 
-        self.horizontalLayout_3.addLayout(self.verticalLayout_2)
+        self.horizontalLayout_3.addWidget(self.labelState)
 
-        self.pgsBar = QProgressBar(self.layoutWidget)
-        self.pgsBar.setObjectName(u"pgsBar")
-        self.pgsBar.setValue(24)
-
-        self.horizontalLayout_3.addWidget(self.pgsBar)
-
-        self.btnStart = QPushButton(self.layoutWidget)
+        self.btnStart = QPushButton(self.widget)
         self.btnStart.setObjectName(u"btnStart")
 
         self.horizontalLayout_3.addWidget(self.btnStart)
 
-        self.horizontalLayout_3.setStretch(0, 8)
-        self.horizontalLayout_3.setStretch(2, 2)
-        self.horizontalLayout_3.setStretch(3, 3)
+        self.horizontalLayout_3.setStretch(0, 4)
+        self.horizontalLayout_3.setStretch(1, 6)
+        self.horizontalLayout_3.setStretch(3, 1)
         self.horizontalLayout_3.setStretch(4, 1)
+        self.horizontalLayout_3.setStretch(6, 1)
 
         self.retranslateUi(Form)
 
@@ -117,9 +104,9 @@ class Ui_Form(object):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
         self.labelFileName.setText(QCoreApplication.translate("Form", u"filename", None))
         self.labelVideoInfo.setText(QCoreApplication.translate("Form", u"filename", None))
-        self.checkBoxKeepRatio.setText(QCoreApplication.translate("Form", u"\u4fdd\u6301\u7eb5\u6a2a\u6bd4", None))
         self.label_3.setText(QCoreApplication.translate("Form", u"\u7f29\u653e\u6bd4", None))
         self.label_4.setText(QCoreApplication.translate("Form", u"x", None))
+        self.labelState.setText(QCoreApplication.translate("Form", u"TextLabel", None))
         self.btnStart.setText(QCoreApplication.translate("Form", u"\u5f00\u59cb", None))
     # retranslateUi
 
--- video_reisze_task_item.py
+++ video_reisze_task_item.py
@@ -1,22 +1,46 @@
 from ui.video_resize_task_item import Ui_Form
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QWidget, QMessageBox
 import ffmpeg
 import os
+import time
+from threading import Thread
 
 
 class VideoResizeTaskItem(QWidget, Ui_Form):
+
+    ratio_choice = ['自定义', '3/4', '1/2', '1/3', '1/4', '1/5', '1/8', '1/10', '1/20']
+
     def __init__(self, filepath):
         super().__init__()
         self.setupUi(self)
-        s = ffmpeg.probe(filepath, 'ffprobe')
-        self.labelFileName.setText(os.path.basename(filepath))
-        # self.labelVideoInfo.setText()
-        self.pgsBar.setValue(0)
-        self.get_video_info(s)
+        self.filepath = filepath
+        self.video_info = ffmpeg.probe(self.filepath, 'ffprobe')
+        self.w = 0
+        self.h = 0
+        self.init_ui()
+
+    def init_ui(self):
+        self.labelFileName.setText(os.path.basename(self.filepath))
+        self.get_video_info(self.video_info)
+        self.add_ratios()
+        self.labelState.setText('未开始')
+
+        self.comboBoxRatio.currentIndexChanged.connect(self.on_ratio_selected)
+        self.btnStart.clicked.connect(self.on_start_clicked)
+
+    def update_progress_bar(self, p):
+        self.labelState.setText('进行中')
+        while True:
+            state = p.poll()
+            if state is not None:
+                if state == 0:
+                    self.labelState.setText('已完成')
+                else:
+                    self.labelState.setText('任务失败')
+                break
+            time.sleep(1)
 
     def get_video_info(self, s):
-        w = 0
-        h = 0
         fps = 0
         size = 0
         duration = 0
@@ -30,11 +54,53 @@ class VideoResizeTaskItem(QWidget, Ui_Form):
         if 'streams' in s:
             for item in s.get('streams'):
                 if item['codec_type'] == 'video':
-                    w = item['width']
-                    h = item['height']
+                    self.w = int(item['width'])
+                    self.h = int(item['height'])
                     codec_name = item['codec_name']
                     fps = round(float(item['nb_frames'])/float(item['duration']))
                     break
         size = f"{round(size/1024/1024,2)}Mb"
-        info = f"{w}x{h},{duration}秒,{fps}fps,{codec_name},{size}"
+        info = f"{self.w}x{self.h},{duration}秒,{fps}fps,{codec_name},{size}"
         self.labelVideoInfo.setText(info)
+
+    def add_ratios(self):
+        for ratio in self.ratio_choice:
+            self.comboBoxRatio.addItem(ratio)
+
+    def on_ratio_selected(self):
+        ratio = self.comboBoxRatio.currentText()
+        if ratio == '自定义':
+            self.lineEditResizeW.setEnabled(True)
+            self.lineEditResizeH.setEnabled(True)
+        else:
+            self.lineEditResizeW.setDisabled(True)
+            self.lineEditResizeH.setDisabled(True)
+            r = eval(ratio)
+            r_w = round(r * self.w)
+            r_h = round(r * self.h)
+            if r_w % 2 == 1:
+                r_w += 1
+            if r_h % 2 == 1:
+                r_h += 1
+
+            self.lineEditResizeW.setText(str(r_w))
+            self.lineEditResizeH.setText(str(r_h))
+
+    def on_start_clicked(self):
+        if not self.lineEditResizeW.text() or not self.lineEditResizeH.text():
+            QMessageBox.warning(self, "warning", "请输入正确的缩放宽高", QMessageBox.Close)
+            return
+        r_w = int(self.lineEditResizeW.text())
+        r_h = int(self.lineEditResizeH.text())
+        input_video = ffmpeg.input(self.filepath)
+        v = input_video.video.filter('scale', f'{r_w}x{r_h}')
+        a = input_video.audio
+        suf = '.' + os.path.basename(self.filepath).split('.')[-1]
+        out_path = os.path.join(
+            os.path.dirname(self.filepath),
+            self.filepath.replace(suf, f'_resize_{r_w}x{r_h}{suf}')
+        )
+        out = ffmpeg.output(v, a, out_path).overwrite_output()
+        p = out.run_async()
+        t = Thread(target=self.update_progress_bar, args=(p,))
+        t.start()
